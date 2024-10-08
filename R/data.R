6374637f098781acb9f9463c7455de016d689b0e
Bob Verity <bobverity1@gmail.com>
2024-10-08 21:52:23 +0100

    tweaks and setting up code coverage and other actions

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Example Input Dataset
+#'
+#' A list containing the three data.frames that make up a STAVE object. These
+#' are correctly formatted, so can be used as a reference to when formatting
+#' data. The studies themselves come from a much reduced version of the WWARN
+#' data on antimalaria resistance.
+#'
+#' @format A list containing three data.frames
+#'
+#' @examples
+#' data("example_input", package = "STAVE")
+"example_input"
